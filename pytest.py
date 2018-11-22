322cdc6a4765002f99b15d82fe81d6bb93db7885
Katya Grohotova <katya.grohotova.4@gmail.com>
2018-11-22 10:32:00 +0700

    prosto def

--- pytest.py
+++ pytest.py
@@ -1,11 +1,15 @@
 import unittest
 import Hill
 
-class My_test(unittest.TestCase):
+# class My_test(unittest.TestCase):
+#
+#     def test_norm(self):
+#         res = Hill.add_text(['a'], 7)
+#         self.assertEquals(res,['a', 33, 33, 33, 33, 33, 33])
 
-    def test_norm(self):
-        res = Hill.add_text(['a'], 7)
-        self.assertEquals(res,['a', 33, 33, 33, 33, 33, 33])
+# if __name__ == '__main__':
+#     unittest.main()
 
-if __name__ == '__main__':
-    unittest.main()
+def test_norm(self):
+    res = Hill.add_text(['a'], 7)
+    self.assertEquals(res, ['a', 33, 33, 33, 33, 33, 33])
